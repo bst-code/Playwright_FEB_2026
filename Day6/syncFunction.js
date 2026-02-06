490d3d0a22e95f913ee24e49c6dbfbec565b6473
bst-code <bsparksoftwaretacademy@gmail.com>
2026-02-06 07:04:30 +0530

    sync and async functions

---
+++ Day6/syncFunction.js
@@ -0,0 +1,14 @@
+function demo()
+{
+    console.log("Line 1");
+    console.log("Line 2");
+    console.log("Line 3");
+    console.log("Line 4");
+    setTimeout(()=>{
+         console.log("Line 5");
+    }, 5000)
+   
+    console.log("Line 6"); 
+}
+
+demo()
