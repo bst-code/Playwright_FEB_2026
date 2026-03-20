bbb8a320b77668bb81a83940529c16d606a9cd40
bst-code <bsparksoftwaretacademy@gmail.com>
2026-03-20 07:18:18 +0530

    Cucumber Hooks and customworld Concept

---
+++ CucumberFramework_setup/src/test/utils/world.ts
@@ -0,0 +1,11 @@
+import { setWorldConstructor } from '@cucumber/cucumber';
+import { Browser, Page } from '@playwright/test';
+
+export class CustomWorld {
+
+    browser!: Browser;
+    page!: Page;
+}
+
+setWorldConstructor(CustomWorld);
+
